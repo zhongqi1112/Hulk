f2d4cc3e0a390d8a98f84b74c03efb36d443c1f0
zhongqi1112 <zhongqi1112@gmail.com>
2021-01-07 00:30:13 -0800

    2020/01/06

--- leetcode/1.two-sum.py
+++ leetcode/1.two-sum.py
@@ -7,12 +7,16 @@
 # @lc code=start
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # create a dict (hashtable)
         seen = {}
+        # go through the list once
         for count, item in enumerate(nums):
             complement = target - item
             if complement in seen:
-                return [seen[complement], count]
+                indices = [seen[complement], count]
+                return indices
             else:
                 seen[item] = count
+
 # @lc code=end
 
