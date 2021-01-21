99179e84587aa80b5da9ec3e21481d2df7ca4b29
zhongqi1112 <zhongqi1112@gmail.com>
2021-01-20 20:52:35 -0800

    2021/01/20

---
+++ leetcode/704.binary-search.py
@@ -0,0 +1,23 @@
+#
+# @lc app=leetcode id=704 lang=python3
+#
+# [704] Binary Search
+#
+
+# @lc code=start
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            middle = (left + right) // 2
+            if target == nums[middle]:
+                return middle
+            elif target < nums[middle]:
+                right = middle - 1
+            else:
+                left = middle + 1
+        return -1
+        
+# @lc code=end
+
