056924d119b119beb06ccccb330d68a78c0b9968
zhongqi1112 <zhongqi1112@gmail.com>
2021-02-27 12:21:48 -0800

    Create 57.insert-interval.py

---
+++ leetcode/57.insert-interval.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=57 lang=python3
+#
+# [57] Insert Interval
+#
+
+# @lc code=start
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        intervals.append(newInterval)
+        intervals.sort(key=lambda x:x[0])
+        merged = []
+        for interval in intervals:
+            if len(merged) == 0 or merged[-1][1] < interval[0]:
+                merged.append(interval)
+            else:
+                merged[-1][1] = max(merged[-1][1], interval[1])
+        return merged
+# @lc code=end
+
