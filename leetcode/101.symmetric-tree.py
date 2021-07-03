f200fad89bf0b246382488a05f0a04e0e36fb7a2
zhongqi1112 <zhongqi1112@gmail.com>
2021-07-02 23:56:56 -0700

    Update 101.symmetric-tree.py

--- leetcode/101.symmetric-tree.py
+++ leetcode/101.symmetric-tree.py
@@ -12,7 +12,16 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    # https://leetcode.com/problems/symmetric-tree
+    # Updated: 12/25/2020
+    
     def isSymmetric(self, root: TreeNode) -> bool:
-        
+        return self.isMirror(root, root)
+    
+    
+    def isMirror(self, t1, t2):
+        if not t1 and not t2: return True
+        if not t1 or not t2: return False
+        return t1.val == t2.val and self.isMirror(t1.right, t2.left) and self.isMirror(t1.left, t2.right)
 # @lc code=end
 
