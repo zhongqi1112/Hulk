3a0a392eeab7c059f40b4f612aef74eb9629cf0e
zhongqi1112 <zhongqi1112@gmail.com>
2021-07-06 10:29:33 -0700

    Update 101.symmetric-tree.py

--- leetcode/101.symmetric-tree.py
+++ leetcode/101.symmetric-tree.py
@@ -13,15 +13,38 @@
 #         self.right = right
 class Solution:
     # https://leetcode.com/problems/symmetric-tree
-    # Updated: 12/25/2020
+    # Updated: 07/06/2021
+
+    # Recursive
+    # Time complexity: O(n)
+    # Space complexity:O(n)
+    # def isSymmetric(self, root: TreeNode) -> bool:
+    #     return self.isMirror(root, root)
     
+    # def isMirror(self, t1, t2):
+    #     if not t1 and not t2: return True
+    #     if not t1 or not t2: return False
+    #     return t1.val == t2.val and self.isMirror(t1.right, t2.left) and self.isMirror(t1.left, t2.right)
+
+
+    # Iterative
+    # Time complexity: O(n)
+    # Space complexity:O(n)
     def isSymmetric(self, root: TreeNode) -> bool:
-        return self.isMirror(root, root)
-    
-    
-    def isMirror(self, t1, t2):
-        if not t1 and not t2: return True
-        if not t1 or not t2: return False
-        return t1.val == t2.val and self.isMirror(t1.right, t2.left) and self.isMirror(t1.left, t2.right)
+        q = list()
+        q.append(root)
+        q.append(root)
+        while q:
+            t1 = q.pop()
+            t2 = q.pop()
+            if not t1 and not t2: continue
+            if not t1 or not t2: return False
+            if t1.val != t2.val: return False
+            q.append(t1.left)
+            q.append(t2.right)
+            q.append(t1.right)
+            q.append(t2.left)
+        return True
+
 # @lc code=end
 
