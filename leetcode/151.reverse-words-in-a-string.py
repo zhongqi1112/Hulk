6e85027a499da391ac8dccb8a2ee4752c036f98a
zhongqi1112 <zhongqi1112@gmail.com>
2021-07-09 13:34:15 -0700

    Create 151.reverse-words-in-a-string.py

---
+++ leetcode/151.reverse-words-in-a-string.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=151 lang=python3
+#
+# [151] Reverse Words in a String
+#
+
+# @lc code=start
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        return " ".join(reversed(s.split()))
+# @lc code=end
+
